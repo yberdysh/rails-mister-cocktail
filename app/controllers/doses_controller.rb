4790504d1c126d35d539050b55084825d7c7c4df
Jane Costa <yberdysh@gmail.com>
2017-11-02 17:10:42 -0400

    added dose destroy functionality

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,37 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:create, :new]
   def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails do
-    resources :doses, only: [:index]
+    resources :doses, only: [:index, :new, :create, :update, :edit]
   end
+  resources :doses, only: [:destroy]
 end
