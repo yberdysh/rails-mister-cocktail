7085ddea0ce425d70c7fe3e37b4c9fa7b232753b
Jane Costa <yberdysh@gmail.com>
2017-11-19 20:39:28 -0500

    added favicon

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -6,7 +6,7 @@ class Dose < ApplicationRecord
     ["1/8", "1/4", "1/3", "1/2", "2/3", "3/4", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
     allow_nil: false}, presence: true
   validates :measurement, inclusion: { in:
-    ["oz", "tbs", "tsp", "pinch", "dash", "piece", "cube", "leaf", "slice"],
+    ["oz", "tbs", "tsp", "pinch", "dash", "piece", "cube", "cup", "leaf", "slice"],
     allow_blank: true}, presence: true
 end
 
